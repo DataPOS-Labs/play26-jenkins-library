0ccf96396032e8cf0bc4c112f889d1cb3034990f
Haolin Jiang <haolinj@gmail.com>
2020-09-02 16:00:40 +1000

    Init.

---
+++ README.md
@@ -0,0 +1,3 @@
+# play26-jenkins-library
+
+Jenkins library to build Scala `Play Framework 2.6` project with `sbt 1`. 
---
+++ vars/play26BuilderPersistentVolumes.groovy
@@ -0,0 +1,19 @@
+def call(Map config) {
+  return [
+    [
+      path: '/home/jenkins/.ivy2',
+      claimName: "${config.project}-home-jenkins-ivy2",
+      sizeGiB: 2
+    ],
+    [
+      path: '/home/jenkins/.cache/coursier/v1',
+      claimName: "${config.project}-home-jenkins-coursier",
+      sizeGib: 2
+    ],
+    [
+      path: '/home/jenkins/.sbt',
+      claimName: "${config.project}-home-jenkins-sbt",
+      sizeGiB: 2
+    ]
+  ]
+}
