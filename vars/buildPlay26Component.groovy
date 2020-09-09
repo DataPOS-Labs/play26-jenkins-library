3999ac13e595cecf5d6f86bad0803ce1c599460e
Haolin Jiang <haolinj@gmail.com>
2020-09-09 19:32:23 +1000

    QFXFB-635 add s3 uopload path

--- vars/buildPlay26Component.groovy
+++ vars/buildPlay26Component.groovy
@@ -99,7 +99,7 @@ def call(Map config) {
       withAWS(credentials: "${config.awsCredentialId}") {
         s3Upload(
           bucket: "${config.bucket}",
-          path: "",
+          path: "/${config.project}/${config.buildNumber}",
           file: tarName
         )
       }
